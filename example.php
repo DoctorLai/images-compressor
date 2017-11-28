5a202afd951ec28ac214dcca4429bb67736db8c8
@justyy <oh@steakovercooked.com>
2017-11-28 13:58:13 +0000

    Update example.php

--- example.php
+++ example.php
@@ -1,4 +1,8 @@
 <?php
+  require('class-imagescompressor.php');
+  $app_key = 'YOUR_APP_KEY';
+  $app_secret = 'YOUR_APP_SECRET';
+
   $obj = new ImagesCompressor($app_key, $app_secret);
   print_r($obj->check());
   $response = $obj->optimize("/var/www/imagerecycle/test.jpg");
